029a79a201fdd047a420f603e498708a965e0b8e
Nicholas Vuono <nhv5@scarletmail.rutgers.edu>
2016-09-23 01:24:08 -0400

    Final Apple Picker Draft
    Complete apple picker tutorial

--- Assets/Apple.cs
+++ Assets/Apple.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Apple : MonoBehaviour {
 
+    public static float bottomY = -20f;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +12,14 @@ public class Apple : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        if(transform.position.y < bottomY)
+        {
+            Destroy(this.gameObject);
+
+            ApplePicker apScript = Camera.main.GetComponent<ApplePicker>();
+            apScript.AppleDestroyed();
+
+        }
 	
 	}
 }
---
+++ Assets/HighScore.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+//also used the TEXT fix from working with Kevin on this portion as well
+
+
+public class HighScore : MonoBehaviour {
+
+    static public int score = 1000;
+
+    void Awake()
+    {
+        if (PlayerPrefs.HasKey("ApplePickerHigheScore"))
+        {
+            score = PlayerPrefs.GetInt("ApplePickerHighScore");
+        }
+        PlayerPrefs.SetInt("ApplePickerHighScore", score);
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Text gt = this.GetComponent<Text>();
+        gt.text = "High Score: " + score;
+
+        if(score > PlayerPrefs.GetInt("ApplePickerHighScore"))
+        {
+            PlayerPrefs.SetInt("ApplePickerHighScore", score);
+        }
+	}
+}
